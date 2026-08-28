37c48266991e58b83a5e1ae00247556fc460f2a1
Lee Yuchan <yclee1081@gmail.com>
2026-08-28 23:06:41 +0900

    fix i18n

--- astro.config.ts
+++ astro.config.ts
@@ -29,8 +29,8 @@ export default defineConfig({
     }),
   ],
   i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+    locales: ["ko"],
+    defaultLocale: "ko",
     routing: {
       prefixDefaultLocale: false,
     },
