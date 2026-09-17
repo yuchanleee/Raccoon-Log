39983b889aad410f8356ca54e6c6a716c3dc2543
Lee Yuchan <yclee1081@gmail.com>
2026-09-17 13:56:55 +0900

    elementary num 1, projects 1,2

--- src/data/categoryOrder.ts
+++ src/data/categoryOrder.ts
@@ -1,7 +1,7 @@
 export const categoryOrder: Record<string, string[]> = {
     root: ["Math", "Computer Science", "3D printer", "Projects", "Daily Life", "Piano", "others"],
 
-    "Math": ["analysis", "linear-algebra", "differential equation", "Calculus"],
+    "Math": ["analysis", "linear-algebra", "differential equation", "Calculus", "Elementary number theory"],
 
     "Computer Science": ["Web", "ML"],
 
